8e66578aecd113445e114e7199c7ee79fe960aff
Rails Udemy Course DB <example@example.com>
2019-01-10 08:19:47 -0600

    Another Heroku fix attempt

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:id, :title, :description)
+      params.require(:article).permit(:id, :title, :description, category_ids:[])
     end
 
     def require_same_user
